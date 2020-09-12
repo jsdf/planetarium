e787e28627257ec0aac799ecf5389da22227980b
James Friend <james@kitefire.com>
2020-09-12 12:12:22 -0400

    update docs

---
+++ docs/precache-manifest.290e1a3178b26a904d88b90947b7b7b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3dcf28eca4c92e3cfd3d274630fb11",
+    "url": "/planetarium/index.html"
+  },
+  {
+    "revision": "e4dc0dada9b69004a38a",
+    "url": "/planetarium/static/css/main.eb447ccb.chunk.css"
+  },
+  {
+    "revision": "690a629a4cb707cbed2a",
+    "url": "/planetarium/static/js/2.baaba269.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/planetarium/static/js/2.baaba269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dc0dada9b69004a38a",
+    "url": "/planetarium/static/js/main.8b86a68e.chunk.js"
+  },
+  {
+    "revision": "5943755118c18e270684",
+    "url": "/planetarium/static/js/runtime-main.0232097f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/planetarium/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f090293850db16db0883c1edbfb2a5a5.js"
+  "/planetarium/precache-manifest.290e1a3178b26a904d88b90947b7b7b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/planetarium/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
